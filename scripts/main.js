b8b667c228d5cde8a665842cb843ba8b4d995d5e
Sam Mitchell <samuel.i.mitchell@gmail.com>
2023-09-15 12:01:07 -0400

    made functions into consts in JS

--- scripts/main.js
+++ scripts/main.js
@@ -8,38 +8,46 @@ document.addEventListener("DOMContentLoaded", function () {
     const chuckQuote = document.querySelector('#chuckQuote');
 
     const apiUrl = 'https://api.chucknorris.io/jokes/random?category=dev';
-    get(apiUrl).then(function (response) {
-        console.log('Response: ', response);
-        showQuote(response.value, chuckQuote);
-    });
 
     const categoriesUrl = 'https://api.chucknorris.io/jokes/categories';
     get(categoriesUrl).then(function (response) {
         console.log(response);
         makeCategoryList(response);
     })
+
+    categoryListFormEl.addEventListener('submit', function (event) {
+        event.preventDefault();
+        const newCategory = this.querySelector('select').value;
+        const apiUrl = `https://api.chucknorris.io/jokes/random?category=${newCategory}`;
+        generateQuote(apiUrl);
 });
 
-function showQuote(quote, element) {
-    element.innerText = quote;
-}
+    function generateQuote(apiUrl) {
+        get(apiUrl).then(function (response) {
+            console.log('Response: ', response);
+            showQuote(response.value, chuckQuote);
+        });
+    }
 
-function makeCategoryList(categoryArray) {
-    console.log(categoryArray);
-    const selectEl = document.createElement('select');
-    categoryArray.map(function (category) {
-        // create an option element
-        const option = document.createElement('option');
-        // define option attributes
-        option.value = category;
-        option.text = category;
-        // append the option to the <select>
-        selectEl.appendChild(option);
-    });
-    // append the <select> to the <form>
-    categoryListFormEl.append(selectEl);
-}
+    function showQuote(quote, element) {
+        element.innerText = quote;
+    }
 
-categoryListFormEl.addEventListener('submit', {
-
+    function makeCategoryList(categoryArray) {
+        console.log(categoryArray);
+        const selectEl = document.createElement('select');
+        categoryArray.map(function (category) {
+            // create an option element
+            const option = document.createElement('option');
+            // define option attributes
+            option.value = category;
+            option.text = category;
+            // append the option to the <select>
+            selectEl.appendChild(option);
+        });
+        // append the <select> to the <form>
+        categoryListFormEl.append(selectEl);
+    }
 });
+
+// this returns an array makeCategoryList(categoryArray).  16 items.  Our mission is to write a .filter onto this array in order to take out unwanted categories, such as explicit.  Start by removing one category, then move on from there.
